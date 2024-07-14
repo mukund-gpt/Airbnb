94cfcadd31f271df13e19634c47d01c00fed8ecf
mukund-gpt <abhishek.jupyter@gmail.com>
2024-07-14 21:07:39 +0530

    image upload feature

--- controllers/listings.js
+++ controllers/listings.js
@@ -27,8 +27,11 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
+  let url = req.file.path;
+  let filename = req.file.filename;
   let newlisting = new Listing(req.body.listing);
   newlisting.owner = req.user._id;
+  newlisting.image = { url, filename };
   await newlisting.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
--- routes/listing.js
+++ routes/listing.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingControllers = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAsync(listingControllers.index))
-  .post(isLoggedIn, wrapAsync(listingControllers.createListing));
+  .post(
+    isLoggedIn,
+
+    upload.single("listing[image][url]"),
+    wrapAsync(listingControllers.createListing)
+  );
 
 router.get("/new", isLoggedIn, listingControllers.renderNewForm);
 
