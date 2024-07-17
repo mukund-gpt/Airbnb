e321cdbcd17d075e81a3871b453d5cf45f115c61
mukund-gpt <abhishek.jupyter@gmail.com>
2024-07-17 12:10:30 +0530

    css added

--- public/js/script.js
+++ public/js/script.js
@@ -23,34 +23,55 @@
 })();
 
 // Mouse scroll in Filter
+document.addEventListener("DOMContentLoaded", () => {
+  const filtersContainer = document.querySelector(".filters");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-const filtersContainer = document.querySelector('.filters');
+  // Mouse events
+  filtersContainer.addEventListener("mousedown", (e) => {
+    isDown = true;
+    filtersContainer.classList.add("active");
+    startX = e.pageX - filtersContainer.offsetLeft;
+    scrollLeft = filtersContainer.scrollLeft;
+  });
 
-let isDown = false;
-let startX;
-let scrollLeft;
+  filtersContainer.addEventListener("mouseleave", () => {
+    isDown = false;
+    filtersContainer.classList.remove("active");
+  });
 
-filtersContainer.addEventListener('mousedown', (e) => {
-  isDown = true;
-  filtersContainer.classList.add('active');
-  startX = e.pageX - filtersContainer.offsetLeft;
-  scrollLeft = filtersContainer.scrollLeft;
-});
+  filtersContainer.addEventListener("mouseup", () => {
+    isDown = false;
+    filtersContainer.classList.remove("active");
+  });
 
-filtersContainer.addEventListener('mouseleave', () => {
-  isDown = false;
-  filtersContainer.classList.remove('active');
-});
+  filtersContainer.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - filtersContainer.offsetLeft;
+    const walk = (x - startX) * 3; // Adjust the multiplier to control the scroll speed
+    filtersContainer.scrollLeft = scrollLeft - walk;
+  });
 
-filtersContainer.addEventListener('mouseup', () => {
-  isDown = false;
-  filtersContainer.classList.remove('active');
-});
+  // Touch events
+  filtersContainer.addEventListener("touchstart", (e) => {
+    isDown = true;
+    filtersContainer.classList.add("active");
+    startX = e.touches[0].pageX - filtersContainer.offsetLeft;
+    scrollLeft = filtersContainer.scrollLeft;
+  });
 
-filtersContainer.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - filtersContainer.offsetLeft;
-  const walk = (x - startX) * 3; // The multiplier can be adjusted to control the scroll speed
-  filtersContainer.scrollLeft = scrollLeft - walk;
+  filtersContainer.addEventListener("touchend", () => {
+    isDown = false;
+    filtersContainer.classList.remove("active");
+  });
+
+  filtersContainer.addEventListener("touchmove", (e) => {
+    if (!isDown) return;
+    const x = e.touches[0].pageX - filtersContainer.offsetLeft;
+    const walk = (x - startX) * 3; // Adjust the multiplier to control the scroll speed
+    filtersContainer.scrollLeft = scrollLeft - walk;
+  });
 });
